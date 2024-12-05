2f691128f1dd3134577e65e1b3862edfc313c7df
alexanderdombroski <dom21013@byui.edu>
2024-12-04 18:40:23 -0700

    added a project to project page

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,26 +1,39 @@
 const previous_projects = [
     {
-        timeframe: "",
         title: "",
-        desc: ""
-    }
-]
-const next_projects = [
-    {
-        title: "",
+        url: "",
         timeframe: "",
         desc: "",
         img: "",
         alt: "",
-        contributers: "",
+        contributers: [],
+    },
+    {
+        title: "Halloween Website",
+        url: "https://alexanderdombroski.github.io/halloween/",
+        timeframe: "October 2024",
+        desc: "Halloween Sites were created in preparation for halloween and to show to the high schoolers in Rexburg",
+        img: "images/project_images/halloween.png",
+        alt: "halloween site image",
+        contributers: ["Alex Dombroski", "Ellie Talbot", "Amitai Finlayson"],
     }
-]
+];
+const next_projects = [
+    {
+        title: "",
+        desc: "",
+        timeframe: ""
+    }
+];
 
 
 
 
 
 (function loadPage() {
+    document.querySelector('.upcoming-projects').innerHTML = nextSectionTemplate();
+    document.querySelector('.previous-projects').innerHTML = prevSectionTemplate();
+
 
 })();
 
@@ -30,13 +43,32 @@ const next_projects = [
 
 
 
-function prevSectionTemplate() {
+function nextSectionTemplate() {
+    return `
+        <h2>Upcoming Projects</h2>
+    `
+}
+function nextProjectTemplate() {
     return `
     
     `
 }
-function projectTemplate() {
+function prevSectionTemplate() {
     return `
-    
+        <h2>Past Projects</h2>
+        ${previous_projects.map(prevProjectTemplate).join('')}
+    `
+}
+function prevProjectTemplate(project) {
+    return `
+        <article>
+            <div>
+                <a href="${project.url}"><h3>${project.title}</h3></a>
+                <p>${project.timeframe}</p>
+                <p>${project.contributers.join(", ")}</p>
+                <p>${project.desc}</p>
+            </div>
+            ${project.img ? `<img src="${project.img}" alt="${project.alt}" />` : ""}
+        </article>
     `
 }
