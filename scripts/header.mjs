a1e639334b4dce55e8b80e67ccfae93453a59f9d
Gage <gagedorlando@gmail.com>
2025-03-05 18:28:42 -0700

    worked on setting the header automatically, having active tags on the open page, and setting in a module imported to the main js file

---
+++ scripts/header.mjs
@@ -0,0 +1,26 @@
+export function renderHeader() {
+  const header = document.querySelector('header');
+  header.innerHTML = `<nav><img
+              src="images/whitelogo.svg"
+              alt="Logo for the WDD Society"
+              width="100px"
+              id="nav-logo"
+          />
+          <button type="button" id="menu-button">Menu</button>
+          <a href="/index.html" >Home</a>
+          <a href="/members.html">Members</a>
+          <a href="/projects.html">Projects</a>
+          <a href="/contact.html">Contact</a>
+          <a href="/gallery.html">Gallery</a>
+          <a href="/about.html">About</a>
+        </nav>`;
+
+  const currentUrl = window.location.pathname;
+  const activeLink = document.querySelector(`a[href="${currentUrl}"]`);
+  activeLink.classList.add('active');
+  const menuButton = document.getElementById('menu-button');
+  menuButton.addEventListener('click', () => {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+  });
+}
