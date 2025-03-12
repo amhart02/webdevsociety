a8c13d734c9e9f5936dc9d6168b35d2464502aac
Gage <gagedorlando@gmail.com>
2025-03-12 17:32:46 -0600

    paths are right

--- scripts/header.mjs
+++ scripts/header.mjs
@@ -7,15 +7,15 @@ export function renderHeader() {
               id="nav-logo"
           />
           <button type="button" id="menu-button">Menu</button>
-          <a href="index.html" >Home</a>
-          <a href="members.html">Members</a>
-          <a href="projects.html">Projects</a>
-          <a href="contact.html">Contact</a>
-          <a href="gallery.html">Gallery</a>
-          <a href="about.html">About</a>
+          <a href="./index.html" >Home</a>
+          <a href="./members.html">Members</a>
+          <a href="./projects.html">Projects</a>
+          <a href="./contact.html">Contact</a>
+          <a href="./gallery.html">Gallery</a>
+          <a href="./about.html">About</a>
         </nav>`;
 
-  const currentUrl = window.location.pathname;
+  const currentUrl = window.location.pathname.split('/').pop();
   const activeLink = document.querySelector(`a[href="${currentUrl}"]`);
   activeLink.classList.add('active');
   const menuButton = document.getElementById('menu-button');
