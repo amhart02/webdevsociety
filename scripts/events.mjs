33f669eaea1df00fc8302dff96dc478a40459f6d
GageDorl <gagedorlando@gmail.com>
2025-03-21 16:42:56 -0600

    Update events.mjs

--- scripts/events.mjs
+++ scripts/events.mjs
@@ -1,34 +1,6 @@
 const events = [
     {
         title: 'Society Meeting',
-        date: '2/26/2025',
-        time: '5:30pm',
-        location: 'STC 231',
-        description: 'Discussing upcoming events and projects'
-    },
-    {
-        title: 'Society Meeting',
-        date: '3/5/2025',
-        time: '5:30pm',
-        location: 'STC 231',
-        description: 'Discussing upcoming events and projects'
-    },
-    {
-        title: 'Society Meeting',
-        date: '3/12/2025',
-        time: '5:30pm',
-        location: 'STC 231',
-        description: 'Discussing upcoming events and projects'
-    },
-    {
-        title: 'Society Meeting',
-        date: '3/19/2025',
-        time: '5:30pm',
-        location: 'STC 231',
-        description: 'Discussing upcoming events and projects'
-    },
-    {
-        title: 'Society Meeting',
         date: '3/26/2025',
         time: '5:30pm',
         location: 'STC 231',
